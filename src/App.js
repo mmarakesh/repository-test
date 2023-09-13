f12e3a7b64edf608feb5ac6c182db17c014c99e5
mmarakesh <marinamakarova749@gmail.com>
2023-09-13 13:30:34 +0200

    add picture

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Picture from './Picture';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Picture/>
     </div>
   );
 }
---
+++ src/Picture.js
@@ -0,0 +1,9 @@
+import image from './magnolia.png';
+function Picture() {
+    return(
+        <div>
+        <img src={image}/>
+        </div>
+    )
+}
+export default Picture;
