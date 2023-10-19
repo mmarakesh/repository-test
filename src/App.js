f3cf5b8134ec2157b35518ed02e7eee82a8515ae
mmarakesh <marinamakarova749@gmail.com>
2023-10-19 11:26:32 +0200

    my commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import React from 'react';
+import {
+BrowserRouter as Router,
+Routes,
+Route,
+Link
+} from "react-router-dom";
+import Shop from './Shop';
+import About from './About';
+import Home from './Home';
 import './App.css';
-import Picture from './Picture';
 
 function App() {
+  return <Router>
+<nav>
+  <Link to="/about">About</Link>
+  <Link to="/home">Home</Link>
+  <Link to="/shop">Shop</Link>
+</nav>
 
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <Picture/>
-      <button className="double-border-button">Button</button>
-    </div>
-  );
+<Routes>
+  <Route path="/about" element={<About/>}/>
+  <Route path="/home" element={<Home/>}/>
+  <Route path="/shop" element={<Shop/>}/>
+</Routes>
+</Router>
 }
+
+
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import Picture from "./Picture";
+
+function Home() {
+    return(
+    <div className="App">
+      <h1>Hello World</h1>
+      <Picture/>
+      <button className="double-border-button">Button</button>
+    </div>
+    )
+}
+
+export default Home; 
